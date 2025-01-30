4b905d1a81a8ee911c862508ff5e1db0842d9b83
NGNIGUE Frank Junior <frank-junior.ngnigue@danone.com>
2025-01-30 11:48:13 +0100

    Add sunburst

--- essai_p.py
+++ essai_p.py
@@ -4,15 +4,16 @@ import pandas as pd
 import dash_bio
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/Dash_Bio/Chromosomal/clustergram_brain_cancer.csv')
+df_clust = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/Dash_Bio/Chromosomal/clustergram_brain_cancer.csv')
 
-clust = dash_bio.Clustergram(
-    data=df,
-    column_labels=list(df.columns.values),
-    row_labels=list(df.index),
-    height=800,
-    width=700
-)
+
+
+data_sun = dict(
+    character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+    parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+    value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
+
+
 
 
 df_bar = pd.DataFrame({
@@ -32,6 +33,16 @@ scatter_fig = px.scatter(df_scatter, x='x', y='y', color='category', title='Samp
 
 bar_fig = px.bar(df_bar, x='Fruit', y='Amount', title='Fruit Sales')
 
+sun_fig = px.sunburst(data_sun,names='character',parents='parent',values='value',)
+
+clust = dash_bio.Clustergram(
+    data=df_clust,
+    column_labels=list(df_clust.columns.values),
+    row_labels=list(df_clust.index),
+    height=800,
+    width=700
+)
+
 
 st.header("Scatter Plot")
 st.plotly_chart(scatter_fig)
@@ -39,5 +50,8 @@ st.plotly_chart(scatter_fig)
 st.header("Bar Plot")
 st.plotly_chart(bar_fig)
 
-st.header("Bar Plot")
+st.header("Clustergram")
 st.plotly_chart(clust)
+
+st.header("Sunburst Plot")
+st.plotly_chart(sun_fig)
